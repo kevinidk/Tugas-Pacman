5653e4b4e2fc2a55471c5023f2a69293e5a11bbd
kevinidk <33610021+kevinidk@users.noreply.github.com>
2017-12-13 16:25:47 +0700

    Add files via upload

---
+++ pacman.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+char Board[20][15];
+
+int main () {
+	
+    cout << "# # # # # # # # # # # # # # #" << endl;
+    cout << "#" << Board[0][0] << "  \t" << Board[1][0] << "  \t" << Board[2][0] << Board[3][0] << " \t" << Board[4][0] << " " << Board[5][0] << " #" << endl;
+    cout << "#" << Board[0][1] << "  \t" << Board[1][1] << "  \t" << Board[2][1] << Board[3][1] << " \t" << Board[4][1] << " " << Board[5][1] << " #" << endl;
+    cout << "#" << Board[0][2] << "  \t" << Board[1][2] << "  \t" << Board[2][2] << Board[3][2] << " \t" << Board[4][2] << " " << Board[5][2] << " #" << endl;
+    cout << "#" << Board[0][3] << "  \t" << Board[1][3] << "  \t" << Board[2][3] << Board[3][3] << " \t" << Board[4][3] << " " << Board[5][3] << " #" << endl;
+    cout << "#" << Board[0][4] << "  \t" << Board[1][4] << "  \t" << Board[2][4] << Board[3][4] << " \t" << Board[4][4] << " " << Board[5][4] << " #" << endl;
+    cout << "#" << Board[0][5] << "  \t" << Board[1][5] << "  \t" << Board[2][5] << Board[3][5] << " \t" << Board[4][5] << " " << Board[5][5] << " #" << endl;
+    cout << "#" << Board[0][6] << "  \t" << Board[1][6] << "  \t" << Board[2][6] << Board[3][6] << " \t" << Board[4][6] << " " << Board[5][6] << " #" << endl;
+    cout << "#" << Board[0][7] << "  \t" << Board[1][7] << "  \t" << Board[2][7] << Board[3][7] << " \t" << Board[4][7] << " " << Board[5][7] << " #" << endl;
+    cout << "#" << Board[0][8] << "  \t" << Board[1][8] << "  \t" << Board[2][8] << Board[3][8] << " \t" << Board[4][8] << " " << Board[5][8] << " #" << endl;
+    cout << "#" << Board[0][9] << "  \t" << Board[1][9] << "  \t" << Board[2][9] << Board[3][9] << " \t" << Board[4][9] << " " << Board[5][9] << " #" << endl;
+    cout << "#" << Board[0][10] << "  \t" << Board[1][10] << "  \t" << Board[2][10] << Board[3][10] << " \t" << Board[4][10] << " " << Board[5][10] << " #" << endl;
+    cout << "#" << Board[0][11] << "  \t" << Board[1][11] << "  \t" << Board[2][11] << Board[3][11] << " \t" << Board[4][11] << " " << Board[5][11] << " #" << endl;
+    cout << "#" << Board[0][12] << "  \t" << Board[1][12] << "  \t" << Board[2][12] << Board[3][12] << " \t" << Board[4][12] << " " << Board[5][12] << " #" << endl;
+    cout << "#" << Board[0][13] << "  \t" << Board[1][13] << "  \t" << Board[2][13] << Board[3][13] << " \t" << Board[4][13] << " " << Board[5][13] << " #" << endl;
+    cout << "# # # # # # # # # # # # # # #" << endl;
+    cout << endl << endl;
+}
+
+/*char pacman(int row, int col) {
+	Board[2][6] = "'<";
+}*/
